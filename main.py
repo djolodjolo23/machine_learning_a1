9647c9279e9813c9a44f72d9f48748d578aec30c
Djordje <dd222im@student.lnu.se>
2023-03-29 20:35:17 +0200

    Added csv files, will use panda for data related stuff.

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+f = np.array([[1, 2, 3], [6, 5, 4]])
+zz = pd.read_csv('A1_datasets/microchips.csv')
+print(zz)
+print(f)
