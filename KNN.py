24b330d09daa1beb02728333d58473e5ac926eb7
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-04-04 14:01:18 +0200

    all k values seems to be functional

--- KNN.py
+++ KNN.py
@@ -1,31 +1,15 @@
 import numpy as np
 import pandas as pd
 
+
+def majority_vote(arr):
+    count = np.bincount(arr)
+    if count[1] > count[0]:
+        return 1
+    else:
+        return 0
+
 microchips = pd.read_csv('A1_datasets/microchips.csv', header=None)
-
-
-def euclidian_distance(train_set, test_set):
-    distance = np.sqrt(np.sum((train_set - test_set) ** 2), axis=1)
-    return distance
-
-# testing this out
-def _euclidian_distance(train_set, test_set):
-    distances = []
-    for test_point in test_set:
-        row_distances = []
-        for train_point in train_set:
-            distance = np.sqrt(np.sum((test_point - train_point) ** 2))
-            row_distances.append(distance)
-        distances.append(row_distances)
-    return np.array(distances)
-
-
-def predict(train_set, train_labels, test_set, k):
-    distance = _euclidian_distance(train_set, test_set)
-    nearest_indices = np.argsort(distance)[:k]
-    nearest_labels = train_labels[nearest_indices]
-    return np.bincount(nearest_labels).argmax()
-
 
 TRAIN_data = np.array(microchips.iloc[:, :2].values)
 TRAIN_labels = np.array(microchips.iloc[:, 2].values)
@@ -34,8 +18,38 @@ test_chips = np.array([[-0.3, 1.0],
                        [0.6, 0.0]])
 k_values = [1, 3, 5, 7]
 
-for i, train_data in enumerate(TRAIN_data):
-    print("Chip {}:".format(i + 1))
-    for k in k_values:
-        class_label = predict(TRAIN_data, TRAIN_labels, test_chips, k)
-        print("  k = {}: {}".format(k, "OK" if class_label == 1 else "Fail"))
+distances = np.sqrt(np.sum((test_chips[:, np.newaxis, :] - TRAIN_data)**2, axis=-1))
+sorted_distances = np.sort(distances, axis=1)
+sorted_indices = np.argsort(distances, axis=1)
+sorted_labels = TRAIN_labels[sorted_indices]
+
+#for k in k_values:
+
+
+print("k = 1")
+nearest = sorted_labels[:, 0]
+for i in range(len(test_chips)):
+    coords_str = ', '.join([f"{coord: 0.1f}" for coord in test_chips[i]])
+    label = "OK" if nearest[i] == 1 else "FAIL"
+    print(f"chip{i+1}: [{coords_str}] ==> {label}")
+
+print("k = 3")
+three_closest_neighbors = sorted_labels[:, :3]
+for i in range(len(test_chips)):
+    coords_str = ', '.join([f"{coord: 0.1f}" for coord in test_chips[i]])
+    prediction = majority_vote(three_closest_neighbors[i])
+    print (f"Chip{i+1}: {test_chips[i]} ==> {'OK' if prediction else 'FAIL'}")
+
+print("k = 5")
+three_closest_neighbors = sorted_labels[:, :5]
+for i in range(len(test_chips)):
+    coords_str = ', '.join([f"{coord: 0.1f}" for coord in test_chips[i]])
+    prediction = majority_vote(three_closest_neighbors[i])
+    print (f"Chip{i+1}: {test_chips[i]} ==> {'OK' if prediction else 'FAIL'}")
+
+print("k = 7")
+three_closest_neighbors = sorted_labels[:, :7]
+for i in range(len(test_chips)):
+    coords_str = ', '.join([f"{coord: 0.1f}" for coord in test_chips[i]])
+    prediction = majority_vote(three_closest_neighbors[i])
+    print (f"Chip{i+1}: {test_chips[i]} ==> {'OK' if prediction else 'FAIL'}")
