43f22e9b9deab12f3e07338cbef39e77f6040cf6
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-04-09 19:13:46 +0200

    Plots seems to have correct curve finally

--- KNN_Regression.py
+++ KNN_Regression.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -15,14 +17,15 @@ polynomial = pd.read_csv('A1_datasets/polynomial200.csv', header=None)
 k_values = [1, 3, 5, 7, 9, 11]
 
 # shuffling the dataset
-n_rows = polynomial.shape[0]
-indices = np.random.permutation(n_rows)
+#n_rows = polynomial.shape[0]
+#indices = np.random.permutation(n_rows)
 polynomial = polynomial.to_numpy()
-polynomial = polynomial[indices]
+#polynomial = polynomial[indices]
 
 # splitting the dataset into two pieces for training and testing
-train_data = polynomial[:(len(polynomial) // 2), :]
-test_data = polynomial[(len(polynomial)) // 2:, :]
+#train_data = polynomial[:(len(polynomial) // 2), :]
+#test_data = polynomial[(len(polynomial)) // 2:, :]
+train_data, test_data = np.split(polynomial, 2)
 
 # plotting the original data into two
 
@@ -41,53 +44,10 @@ ax2.set_ylabel('Y')
 
 plt.show()
 '''
+
 x = train_data[:, 0]
 y = train_data[:, 1]
-'''
 
-
-results = np.zeros((len(x), 6))
-counter = 0
-for k in k_values:
-    closest_neighbour = []
-    labels_temp = np.empty(100)
-    for i in range(len(x)):
-        num = x[i-1]
-        distance_test = np.abs(x - num)
-        sorted_indices = distance_test.argsort()
-        if k == 1:
-            closest_indices = sorted_indices[k]
-            closest_x = x[closest_indices]
-            closest_neighbour = y[closest_indices]
-        else:
-            closest_indices = sorted_indices[1:k+1]
-            closest_x = x[closest_indices]
-            closest_neighbour = y[closest_indices]
-            prediction = knn_predict_y(closest_neighbour)
-        labels_temp[i] = closest_neighbour if k == 1 else prediction
-    results[:, counter] = labels_temp.flatten()
-    counter = counter + 1
-
-
-# sorting the y values for k = 1
-ydata = results[:, 0] # here 0 determines the first column in the result array for k=1
-sorted_y = [ydata[i] for i in sorted_indices]
-sorted_x = [x[i] for i in sorted_indices]
-
-#slope, intercept, r_value, p_value, std_err = linregress()
-
-# plot the line
-#plt.plot(sorted_x, sorted_y)
-plt.scatter(x, y, color='blue', label='Points')
-
-# add labels and title
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Polynomial curve through x and y coordinates')
-
-# display the plot
-plt.show()
-'''
 
 
 def knn(k_values, minuend_data, subtrahend_data):
@@ -109,32 +69,102 @@ def knn(k_values, minuend_data, subtrahend_data):
         counter = counter + 1
     return array_full
 
+def calculateMSE(data, regression_data):
+    y_data = np.sort(data)
+    y_regression = np.sort(regression_data)
+    mse = 0
+    data_length = len(y_data)
+    for i in range(data_length):
+        y_t = y_data[i]
+        y_r = y_regression[i]
+        mse += ((y_t-y_r)**2)
+    return mse / data_length
+
+
+def calculateKNNRegression(k):
+
+    # Predict points using kNN-regression
+    points_regression = np.empty(shape=(0, 2))
+    counter = 0
+    for xy_test in test_data:
+        test_x = xy_test[0]
+        predicted_point = predictPoint(train_data, test_x, k)
+        points_regression = np.append(points_regression, [predicted_point], axis=0)
+        counter = counter + 1
+    return points_regression
+
+def predictPoint(train_data, x_test, k):
+    # All distances to the points in train data
+    # from a single x point in test data
+    x = train_data[:, 0]
+    y = train_data[:, 1]
+    distance = np.abs(x - x_test)
+    sorted_indices = np.argsort(distance)
+    # Get k-amount of shortest piints
+    points = np.empty(shape=(0, 2))
+    for i in range(k):
+        closest_point_index = sorted_indices[i]
+        point = (x[closest_point_index], y[closest_point_index])
+        points = np.append(points, [[point[0], point[1]]], axis=0)
+    # Vote to get the most approximate point
+    predicted_y = 0
+    for point in points:
+        y = point[1]
+        predicted_y += y
+    predicted_y = predicted_y / k
+
+    # Predicted point
+    predicted_point = np.array([x_test, predicted_y])
+
+    return predicted_point
+
+    # Returns a list of distances which are sorted from shortest to longest
+    # [distance, index]
+    # distance - flaot value of distance
+    # index - index of the data point in train_data that maps to that distance value
+
+
+    # Method
+
 
 train_distances = knn(k_values, train_data, train_data)
-test_distances = knn(k_values, test_data, test_data)
+test_distances = knn(k_values, test_data, train_data)
 
+x_test = test_data[:, 0]
+y_test = test_data[:, 1]
+sort = np.argsort(x_test)
+x_test_sorted = x_test[sort]
+y_test_sorted = y_test[sort]
+
+x_train = x
+y_train = y
+sort_idx = np.argsort(x_train)
+x_train_sorted = x_train[sort_idx]
+y_train_sorted = y_train[sort_idx]
 
 fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(nrows=3, ncols=2, figsize=(15, 15))
 axes = [ax1, ax2, ax3, ax4, ax5, ax6]
 for i, k in enumerate(k_values):
     ax = axes[i]
-    x_train = x
-    y_train = train_distances[:, i]
-    x_test = test_data[:, 0]
-    y_test = test_data[:, 1]
-    sort_idx = np.argsort(x_train)
-    x_train_sorted = x_train[sort_idx]
-    y_train_sorted = y_train[sort_idx]
-    degree = 100
-    z = np.polyfit(x, y_train, degree)
-    p = np.poly1d(z)
-    y_fit = p(x_train_sorted)
-    y_predicted = np.polyval(p, x_test)
-    mse = mean_squared_error(y_test, y_predicted)
-    window_size = 2
-    y_fit_smooth = savgol_filter(y_fit, window_size, 1)
+
+    #x_train = x
+    #y_train = y
+
+
+    #sort_idx = np.argsort(x_train)
+    #x_train_sorted = x_train[sort_idx]
+    #y_train_sorted = y_train[sort_idx]
+
+    r = calculateKNNRegression(k)
+
+
+    mse = calculateMSE(y_train, r)
+    #mse = round(mse, 2)
+    sorted_regression_data = r[r[:, 0].argsort()]
+    x_regression = np.array(sorted_regression_data[:, 0])
+    y_regression = np.array(sorted_regression_data[:, 1])
     ax.plot(x_train_sorted, y_train_sorted, 'o')
-    ax.plot(x_train_sorted, y_fit_smooth, label='Fitted function')
+    ax.plot(x_regression, y_regression, 'r-')
     ax.set_title("K = " + str(k) + " , MSE: " + str(mse))
 plt.subplots_adjust(hspace=0.3, wspace=0.3)
 plt.show()
